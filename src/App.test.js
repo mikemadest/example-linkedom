676be05a94c4a7bdef1cbdae48e1ee5167b787ea
Mickael Meausoone <Mickael.Meausoone@hmhco.com>
2021-07-11 16:53:51 +0100

    Axe test added + RegExp problem demonstration
    To compare jsdom and linkedom:
    
    npm run testjsdom src/App.test.js
    npm test src/App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,31 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+"use strict";
+import { render, screen } from "@testing-library/react";
+import { axe, toHaveNoViolations } from "jest-axe";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
+expect.extend(toHaveNoViolations);
 
-  // regExp search is not working for now with linkedom (probably jest-environment-linkedom issue, maybe getRootNode?)
-  //expect(screen.getByText(/learn react/i)).toBeInTheDocument();
 
-  expect(screen.getByText('Learn React')).toBeInTheDocument();
+describe("Example App tests", () => {
+  it("pass Axe tests", async () => {
+    const { container } = render(<App />);
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+
+  it("renders content", () => {
+    render(<App />);
+
+
+    // quick experiment: with jsdom matcher is an instance of RegExp but not with linkedom
+    const matcher = /learn react/i;
+    console.log('matcher.constructor.name = ', matcher.constructor.name); // always RegExp as you would expect
+    console.log(`matcher is ${matcher instanceof RegExp ? 'a' : 'NOT'} RegExp`);
+
+    expect(
+      screen.getByText(/learn react/i, { exact: false }) // by using exact false it WILL do matcher.test(text) as expected in testing-library-dom
+    ).toBeInTheDocument();
+
+    expect(screen.getByText("Learn React")).toBeInTheDocument();
+  });
 });
